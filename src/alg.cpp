1358c5a94149bf3c50d3ea707d6656a7f52b8ea1
ксюша 3 <tyurinaksen111a@gmail.com>
2026-03-30 18:41:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,7 +5,7 @@
 
 double pown(double value, uint16_t n) {
   double rez = 1.0;
-  for (uint16_t i = 0; i < n; i++) {
+  for (uint16_t i = 0; i < n; ++i) {
       rez = rez * value;
   }
   return rez;
@@ -13,32 +13,32 @@ double pown(double value, uint16_t n) {
 
 uint64_t fact(uint16_t n) {
   uint64_t rez = 1;
-  for (uint64_t i = 1; i <= n; i++) {
+  for (uint64_t i = 1; i <= n; ++i) {
     rez = rez * i;
   }
   return rez;
 }
 
 double calcItem(double x, uint16_t n) {
-  double a = pown(x,n);
+  double a = pown(x, n);
   double b = fact(n);
   return a / b;
 }
 
 double expn(double x, uint16_t count) {
-  double rez = 0;
-  for (uint16_t i = 0; i < count; i++) {
-    rez = rez + (pown(x,i)/fact(i));
+  double rez = 0.0;
+  for (uint16_t i = 0; i <= count; ++i) {
+    rez = rez + (pown(x, i) / fact(i));
   }
   return rez;
 }
 
 double sinn(double x, uint16_t count) {
   double sum = 0.0;
-  for (uint16_t n = 0; n < count; n++) {
+  for (uint16_t n = 0; n <= count; ++n) {
     uint16_t pow = 2 * n + 1;
-    double a = calcItem(x,pow);
-    if (n % 2 == 0){
+    double a = calcItem(x, pow);
+    if (n % 2 == 0) {
       sum += a;
     } else {
       sum -= a;
@@ -49,10 +49,10 @@ double sinn(double x, uint16_t count) {
 
 double cosn(double x, uint16_t count) {
   double sum = 0.0;
-  for (uint16_t n = 0; n < count; n++) {
-    uint16_t pow = 2*n;
-    double a = calcItem(x,pow);
-    if (n % 2 == 0){
+  for (uint16_t n = 0; n <= count; ++n) {
+    uint16_t pow = 2 * n;
+    double a = calcItem(x, pow);
+    if (n % 2 == 0) {
       sum += a;
     } else {
       sum -= a;
